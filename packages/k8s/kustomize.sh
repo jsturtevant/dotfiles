bd9c0b0b58d0edc0f6394693b4a6a60bda5fdd70
James Sturtevant <jsturtevant@gmail.com>
2022-01-15 16:06:26 -0800

    fix customize install

--- packages/k8s/kustomize.sh
+++ packages/k8s/kustomize.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+kustomize=$(mktemp -d)
+curl -L -o $kustomize/kustomize.tar.gz  https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.4.1/kustomize_v4.4.1_linux_amd64.tar.gz
+sudo tar -C /usr/local/bin -xzf $kustomize/kustomize.tar.gz
